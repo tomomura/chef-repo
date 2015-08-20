329bb34e1dbc6f46fae923a2feed1e38ab9634c0
tomomura <am.n04a@gmail.com>
2015-08-20 16:17:23 +0900

    porgのインストールレシピの追加

--- site-cookbooks/porg/recipes/default.rb
+++ site-cookbooks/porg/recipes/default.rb
@@ -7,22 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file "tmp/#{node[:porg][:arch_file_name]}" do
-  source node['porg']['url']
-  checksum "#{node[:porg][:checksum]}"
+remote_file "/tmp/#{node[:porg][:arch_file_name]}" do
+  source node[:porg][:url]
+  action :create
   notifies :run, 'execute[Install porg from source]', :immediately
 end
 
 execute "Install porg from source" do
   action :nothing
-  prefix = node['porg']['prefix'] ? "prefix=#{node['porg']['prefix']}" : nil
+  not_if "which porg"
+  prefix = node['porg']['prefix'] ? "--prefix=#{node['porg']['prefix']}" : nil
   command <<-COMMAND
     cd /tmp
     tar zxvf #{node[:porg][:arch_file_name]}
     cd #{::File.basename(node[:porg][:arch_file_name], '.tar.gz')}
-    ./configure #{"--disable-grop" if node[:porg][:grop]}
-    make #{prefix}
-    make install #{prefix}
+    ./configure #{"--disable-grop" if !node[:porg][:grop]} #{prefix}
+    make
+    sudo make install
     sudo make logme
   COMMAND
 end
